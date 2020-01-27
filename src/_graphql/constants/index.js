74e667778fae9c0ff2a1ef35a0b7e69b6add7df7
Tigran Keshishyan <tigran.keshishyan@genesis.ae>
2020-01-27 17:58:53 +0400

    Remove published date restriction

--- src/_graphql/constants/index.js
+++ src/_graphql/constants/index.js
@@ -1,17 +1,5 @@
-import moment from 'moment';
-
-const dateFormat = 'MM-DD-YYYYTHH:mm:ss';
-const delayMinutes = -25;
-
 export const defaultFilter = {
   published: {
     equalTo: true,
-  },
-  publishedAt: {
-    // Exclude posts which were published {delayMinutes} minutes ago
-    // it is average time for updating the server
-    lessThanOrEqualTo: moment(new Date(), dateFormat)
-      .add(delayMinutes, 'minute')
-      .toISOString(),
   },
 };
