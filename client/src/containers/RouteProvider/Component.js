e811ad9e9654798bb6f1a681aaa5a22edc27becb
Tigran Keshishyan <tigran.keshishyan@genesis.ae>
2020-02-04 10:22:14 +0400

    Fix analytics tools event triggers

--- client/src/containers/RouteProvider/Component.js
+++ client/src/containers/RouteProvider/Component.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 import ReactGoogleAnalytics from 'react-ga';
+import { useHistory } from 'react-router-dom';
 import ReactFacebookPixel from 'react-facebook-pixel';
 
 let lastLocation = '/';
 
 const transitionDelay = 300;
 
-const googleAnalyticsTrackId = 'UA-43195883-2';
 const fbPixelId = '557862558318761';
+const googleAnalyticsTrackId = 'UA-43195883-2';
 
-ReactGoogleAnalytics.initialize(googleAnalyticsTrackId);
 ReactFacebookPixel.init(fbPixelId);
+ReactGoogleAnalytics.initialize(googleAnalyticsTrackId);
 
 function RouteProvider(props) {
   const history = useHistory();
@@ -21,16 +21,18 @@ function RouteProvider(props) {
       const isNewLocation = lastLocation !== pathname;
       lastLocation = pathname;
 
-      // Scroll the view to the top if the route was changed
-      if (isNewLocation && window.scrollY > 0) {
-        window.setTimeout(() => window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
-        }), transitionDelay);
-
+      if (isNewLocation) {
         ReactGoogleAnalytics.set({ page: pathname }); // Update the user's current page
         ReactGoogleAnalytics.pageview(pathname); // Record a page view for the given page
         ReactFacebookPixel.pageView();
+
+        // Scroll the view to the top if the route was changed
+        if (window.scrollY > 0) {
+          window.setTimeout(() => window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          }), transitionDelay);
+        }
       }
 
       return lastLocation;
