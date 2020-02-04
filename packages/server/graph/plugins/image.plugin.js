4fb6b0bcfdcd4dbc79b83329c7e1a5ccc3a42c18
hsanoyan <hovosanoyan@gmail.com>
2020-02-04 19:56:57 +0400

    Delete image from S3 after database record removed

--- packages/server/graph/plugins/image.plugin.js
+++ packages/server/graph/plugins/image.plugin.js
@@ -1,6 +1,8 @@
 import GraphileUtils from 'graphile-utils'
 
 import { anmHost } from '../../../config'
+import { deleteImage } from '../../utils/storage.util'
+import { Slack } from '../../utils/slack.util'
 
 const { makeWrapResolversPlugin } = GraphileUtils
 
@@ -8,6 +10,22 @@ export default makeWrapResolversPlugin({
   Image: {
     async url (resolve) {
       return `${anmHost}/${await resolve()}`
+    }
+  },
+  Mutation: {
+    deleteImage: {
+      requires: { childColumns: [{ alias: 'url', column: 'url' }] },
+      async resolve (resolve) {
+        const resolved = await resolve()
+
+        const { url } = resolved.data
+
+        await deleteImage(url).catch(err => {
+          Slack.devMessage(`failed to remove image from s3 ${url}. ${err.message}`)
+        })
+
+        return resolved
+      }
     }
   }
 })
