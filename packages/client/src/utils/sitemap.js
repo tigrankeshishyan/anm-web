085a35c6126947848ed7dfa08e61f8ede973f1e9
hsanoyan <hovosanoyan@gmail.com>
2020-02-05 21:56:17 +0400

    Add gql tag to graphql queries

--- packages/client/src/utils/sitemap.js
+++ packages/client/src/utils/sitemap.js
@@ -1,22 +1,23 @@
-import routes from 'routes';
-import { languages } from 'localization/constants';
-import fetch from 'isomorphic-fetch';
+import routes from "routes";
+import { languages } from "localization/constants";
+import fetch from "isomorphic-fetch";
+import { gql } from "apollo-boost";
 
 const host = process.env.REACT_APP_HOST;
 
 const staticRoutes = routes.filter(route => route.isStatic);
 
-export const getOptions = (query, lang = 'hy') => ({
-  method: 'POST',
+export const getOptions = (query, lang = "hy") => ({
+  method: "POST",
   body: JSON.stringify({ query }),
   headers: {
-    'Content-type': 'application/json',
-    'Accept-Language': lang
+    "Content-type": "application/json",
+    "Accept-Language": lang
   }
 });
 
-export const fetchGraphData = async (options) => {
-  const response = await fetch(host + '/graphql', options);
+export const fetchGraphData = async options => {
+  const response = await fetch(host + "/graphql", options);
   if (response.status >= 200 && response.status < 300) {
     return response.json();
   } else {
@@ -33,18 +34,24 @@ const getSitemapTemplate = content => {
   `;
 };
 
-const getSitemapBlock = ({ url, lastMod, changefreq = 'weekly' }) => {
+const getSitemapBlock = ({ url, lastMod, changefreq = "weekly" }) => {
   return `
     <url>
       <loc>${url}</loc>
-      ${lastMod ? `<lastmod>${new Date(lastMod).toISOString().split('T')[0]}</lastmod>` : ''}
+      ${
+        lastMod
+          ? `<lastmod>${
+              new Date(lastMod).toISOString().split("T")[0]
+            }</lastmod>`
+          : ""
+      }
       <changefreq>${changefreq}</changefreq>
       <priority>0.8</priority>
     </url>
   `;
 };
 
-const articleQuery = `
+const articleQuery = gql`
   query {
     articles (filter: {
     published: {
@@ -60,7 +67,7 @@ const articleQuery = `
   }
 `;
 
-const musiciansQuery = `
+const musiciansQuery = gql`
   query {
     musicians (filter: {
     published: {
@@ -76,7 +83,7 @@ const musiciansQuery = `
  }
 `;
 
-const scoresQuery = `
+const scoresQuery = gql`
   query {
     scores (filter: {
     published: {
@@ -96,43 +103,46 @@ function done(content) {
   console.log(getSitemapTemplate(content));
 }
 
-export default async function generate () {
-  let sitemapContent = '';
+export default async function generate() {
+  let sitemapContent = "";
   let dataLoadedIndex = 0;
   const lengs = Object.keys(languages);
 
   lengs.forEach(async lang => {
-      // Add static pages to sitemap
-      staticRoutes.forEach(route => {
-        const url = host + route.path.replace(':locale', lang);
-        sitemapContent += getSitemapBlock({ url });
-      });
-
-      // Fetch articles, musicians and scores
-      const articleResponse = await fetchGraphData(getOptions(articleQuery, lang));
-      const musiciansResponse = await fetchGraphData(getOptions(musiciansQuery, lang));
-      const scoresResponse = await fetchGraphData(getOptions(scoresQuery, lang));
-
-      articleResponse.data.articles.nodes.forEach(article => {
-        const url = `${host}/${lang}/news/${article.path}/${article.id}`;
-        sitemapContent += getSitemapBlock({ url, lastMod: article.updatedAt });
-      });
-
-      musiciansResponse.data.musicians.nodes.forEach(musician => {
-        const url = `${host}/${lang}/musician/${musician.path}/${musician.id}`;
-        sitemapContent += getSitemapBlock({ url, lastMod: musician.updatedAt });
-      });
-
-      scoresResponse.data.scores.nodes.forEach(score => {
-        const url = `${host}/${lang}/music-sheet-score/${score.path}/${score.id}`;
-        sitemapContent += getSitemapBlock({ url, lastMod: score.updatedAt });
-      });
-
-      ++dataLoadedIndex;
-
-      if (dataLoadedIndex === lengs.length - 1) {
-        done(sitemapContent);
-      }
+    // Add static pages to sitemap
+    staticRoutes.forEach(route => {
+      const url = host + route.path.replace(":locale", lang);
+      sitemapContent += getSitemapBlock({ url });
     });
-}
 
+    // Fetch articles, musicians and scores
+    const articleResponse = await fetchGraphData(
+      getOptions(articleQuery, lang)
+    );
+    const musiciansResponse = await fetchGraphData(
+      getOptions(musiciansQuery, lang)
+    );
+    const scoresResponse = await fetchGraphData(getOptions(scoresQuery, lang));
+
+    articleResponse.data.articles.nodes.forEach(article => {
+      const url = `${host}/${lang}/news/${article.path}/${article.id}`;
+      sitemapContent += getSitemapBlock({ url, lastMod: article.updatedAt });
+    });
+
+    musiciansResponse.data.musicians.nodes.forEach(musician => {
+      const url = `${host}/${lang}/musician/${musician.path}/${musician.id}`;
+      sitemapContent += getSitemapBlock({ url, lastMod: musician.updatedAt });
+    });
+
+    scoresResponse.data.scores.nodes.forEach(score => {
+      const url = `${host}/${lang}/music-sheet-score/${score.path}/${score.id}`;
+      sitemapContent += getSitemapBlock({ url, lastMod: score.updatedAt });
+    });
+
+    ++dataLoadedIndex;
+
+    if (dataLoadedIndex === lengs.length - 1) {
+      done(sitemapContent);
+    }
+  });
+}
