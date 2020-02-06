6f62d57c6fa2025fd04cbe430f336bec510cdc88
hsanoyan <hovosanoyan@gmail.com>
2020-02-06 09:34:14 +0400

    Update apollo.config to include only graphql files

--- apollo.config.js
+++ apollo.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   client: {
     service: {
-      name: 'Local Schema File',
-      localSchemaFile: './schema.graphql'
+      name: "Local Schema File",
+      localSchemaFile: "./schema.graphql"
     },
-    includes: ['./packages/client/**']
+    includes: [
+      "./packages/client/src/_graphql/**",
+      "./packages/client/src/utils/sitemap.js"
+    ]
   }
 };
