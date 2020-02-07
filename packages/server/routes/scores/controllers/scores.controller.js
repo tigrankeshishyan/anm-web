c46b53a26d77f61b7a419e920c77e594a0318a97
hsanoyan <hovosanoyan@gmail.com>
2020-02-07 12:49:39 +0400

    Add score poster endpoint

--- packages/server/routes/scores/controllers/scores.controller.js
+++ packages/server/routes/scores/controllers/scores.controller.js
@@ -1,6 +1,9 @@
 import assert from 'assert'
 import path from 'path'
 
+import mime from 'mime-types'
+import sharp from 'sharp'
+
 import { ExpressError } from '../../../utils/error.util'
 import * as Storage from '../../../utils/storage.util'
 import { scoreComingSoon, scorePreviewName } from '../../../../config'
@@ -65,6 +68,60 @@ export async function getScore (req, res, next) {
     )
 
     doc.pipe(res)
+  } catch (err) {
+    next(err)
+  }
+}
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {Function} next
+ */
+export async function getPoster (req, res, next) {
+  try {
+    const key = Storage.getScorePosterKey(req.params.scoreId)
+    const width = parseInt(req.query.width) || undefined
+    const height = parseInt(req.query.height) || undefined
+    const fit = req.query.fit || undefined
+
+    const ext = path.extname(key) || '.png'
+
+    const trans = sharp().resize(width, height, {
+      fit,
+      background: { r: 0, g: 0, b: 0, alpha: 0 }
+    })
+
+    switch (ext) {
+      case '.jpg':
+        trans.jpeg()
+        break
+      case '.webp':
+        trans.webp()
+        break
+      default:
+        trans.png()
+    }
+
+    res.contentType(mime.lookup(ext))
+
+    Storage.getObject(key)
+      .createReadStream()
+      .on('error', (err) => {
+        if (err.code === 'NoSuchKey') {
+          next(new ExpressError(err.message, 404))
+        } else {
+          next(err)
+        }
+      })
+      .pipe(trans)
+      .on('error', (err) => {
+        next(err)
+      })
+      .pipe(res)
+      .on('error', (err) => {
+        next(err)
+      })
   } catch (err) {
     next(err)
   }
